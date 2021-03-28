4de0fec8e89fb93b6930404bee24294f35166a1f
Sara Nikolic <nikolic.sara14@gmail.com>
2021-03-28 17:17:34 +0300

    Created new features, by using feature/feature1new.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ x = random.randint(1, 100)
 print("You have 5 chances to guess the number between 1 and 100.")
 
 count = 0
-print(x)
 
 while count < 5:
     guess = int(input("Your guess: "))
@@ -12,8 +11,11 @@ while count < 5:
         print("Warning! One more guess left.")
     if count == 4 and x is not guess:
         print("Oops! Game over. The number you failed to guess was: " + str(x))
-        break
     if x == guess:
         print("Congratulations! You guessed the number, and it's: " + str(guess))
         break
+    if guess < x:
+        print("Try with a number bigger than " + str(guess))
+    if guess > x:
+        print("Try with a number smaller than " + str(guess))
     count += 1
